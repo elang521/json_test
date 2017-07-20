a25b1f8c2d0faac725e2d48ae63b264d37d72654
elang521 <ymzhao520@163.com>
2017-07-20 17:21:57 +0800

    屏蔽

--- Json_Test/main.cpp
+++ Json_Test/main.cpp
@@ -3,13 +3,13 @@
 #include <fstream>
 #include <cassert>
 #include "json/json.h"
-#include "led/Led_Manager.h"
+//#include "led/Led_Manager.h"
 //#include "Warning/Warning_Manager.h"
 //#include "Message/Message_Manager.h"
 int main()
 {
-	Led_Manager *m_led_manager = Led_Manager::Get_Instance();
+    //Led_Manager *m_led_manager = Led_Manager::Get_Instance();
     //Warning_Manager * warning = Warning_Manager::GetInstance();
     //Message_Manager * m_Message = Message_Manager::Get_Instance();
-	return 0;
+    return 0;
 }
