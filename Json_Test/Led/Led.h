2d0838c4c9a6769a18f3809b24c679390a0bad56
elang521 <ymzhao520@163.com>
2017-07-27 10:23:57 +0800

    配置文件修改完毕，修改led.h错误

--- Json_Test/Led/Led.h
+++ Json_Test/Led/Led.h
@@ -14,16 +14,8 @@ public:
 	bool common;          		//是否属于公共灯带，true：属于；false：不属于
 	int x;						//横坐标
 	int y;						//纵坐标	
-
-	Precondition precondition;      //前置条件
-	Led_Priority priority;          //优先级
-	bool blink;               		//true:闪烁；flase:持续
-	int cycle;						//闪烁周期（毫秒为单位）	
-    std::string text_cn;  			//中文消息
-    std::string text_en;  			//英文消息
-	std::string sound;      		//声音文件路径
-	std::string	icon;	        	//图标路径Precondition precondition;      //前置条件
-	Led_Priority priority;          //优先级
+	Led_Priority priority;      //优先级
+	Precondition precondition;	//前置条件
 	bool blink;               		//true:闪烁；flase:持续
 	int cycle;						//闪烁周期（毫秒为单位）	
     std::string text_cn;  			//中文消息
